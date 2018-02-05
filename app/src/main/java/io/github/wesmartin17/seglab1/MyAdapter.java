0385dc42dc73c769b6350f7d560722b2d5dc9dea
Mac Hayter <hayterm96@gmail.com>
2018-02-05 14:41:25 -0500

    Summary

---
+++ app/src/main/java/io/github/wesmartin17/seglab1/MyAdapter.java
@@ -0,0 +1,8 @@
+package io.github.wesmartin17.seglab1;
+
+/**
+ * Created by Mac on 2018-02-05.
+ */
+
+public class MyAdapter {
+}
---
+++ app/src/main/java/io/github/wesmartin17/seglab1/SummaryActivity.java
@@ -0,0 +1,13 @@
+package io.github.wesmartin17.seglab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
